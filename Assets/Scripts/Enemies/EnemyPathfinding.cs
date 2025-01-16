fafc34ee8737f5176819d2a7f50c52d9cd1bad2d
Cao Le Huy <Caolehuy1@gmail.com>
2025-01-16 10:54:20 +0700

    code

--- Assets/Scripts/Enemies/EnemyPathfinding.cs
+++ Assets/Scripts/Enemies/EnemyPathfinding.cs
@@ -1,36 +1,46 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    
-
     [SerializeField] private float moveSpeed = 2f;
 
     private Rigidbody2D rb;
     private Vector2 moveDir;
-    private Knockback   knockback;
+    private Knockback knockback;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         knockback = GetComponent<Knockback>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        if (knockback.GettingKnockedBack)
-        {           
-            return; 
+        if (knockback.GettingKnockedBack) { return; }
+
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+
+        if (moveDir.x < 0)
+        {
+            spriteRenderer.flipX = true;
         }
-        rb.MovePosition(rb.position + moveDir *(moveSpeed * Time.fixedDeltaTime));
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
     }
-     public void MoveTo(Vector2 targetPosition)
+
+    public void MoveTo(Vector2 targetPosition)
     {
         moveDir = targetPosition;
     }
 
+    public void StopMoving()
+    {
+        moveDir = Vector3.zero;
+    }
 }
- 
